17bde5fcd7904e127a9e7023f3e842398c0eca9a
github username <github email address>
2019-08-24 13:33:01 -0700

    Done.

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import NavBar from './NavBar'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <React.Fragment>
+          <h1>Home!</h1>
+        </React.Fragment>
+      </div>
+    )
+  }
+}
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import Home from './components/Home'
+import About from './components/About'
+import Login from './components/Login'
+import NavBar from './components/NavBar'
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink} from 'react-router-dom'
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
-
-ReactDOM.render(
-  <Home />,
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      <NavBar />
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login}/>
+    </React.Fragment>
+  </Router>),
   document.getElementById('root')
 );
